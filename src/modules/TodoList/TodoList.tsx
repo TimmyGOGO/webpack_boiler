38148516097ff439893781b2d702cde8b4309804
TimmyGOGO <temich941@mail.ru>
2019-06-22 17:59:24 +0400

    develop: set up additiona plugins for .babel and added Roboto google font

--- src/modules/TodoList/TodoList.tsx
+++ src/modules/TodoList/TodoList.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const StyledTodoListContainer = styled.div``
+export const StyledTodoListContainer = styled.div`
+  height: 100vh;
+  margin: 20px;
+  background: linear-gradient(#fff, #e7f3f1);
+`
+
+const StyledHeadline = styled.div`
+  font-family: Roboto;
+  font-size: 24px;
+  line-height: 30px;
+`
 
 export interface ITodoList {}
 
 export class TodoList extends React.PureComponent<ITodoList> {
+  state = {}
+
   render() {
-    return <StyledTodoListContainer>TodoList component</StyledTodoListContainer>
+    return (
+      <StyledTodoListContainer>
+        <StyledHeadline>TodoList component</StyledHeadline>
+      </StyledTodoListContainer>
+    )
   }
 }
