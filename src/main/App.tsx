a2bbef3ac9b7980fcf15e5998f4fde2c190a1f68
TimmyGOGO <temich941@mail.ru>
2019-06-27 11:23:55 +0300

    issue-1: Grid - added styles

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 import { TodoList } from '../modules/TodoList/TodoList'
 import './globalStyles.css'
+import Grid from '../modules/Grid/Grid'
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <>
-        <TodoList />
+        <Grid />
+        {/* <TodoList /> */}
       </>
     )
   }
