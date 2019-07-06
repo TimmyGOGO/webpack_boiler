c915d48d28c80f2d7de1e9f1a1839ca673716a48
TimmyGOGO <temich941@mail.ru>
2019-07-07 00:46:34 +0400

    issue-1: TrelloLayout - added TrelloLayout [draft]

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -4,13 +4,15 @@ import { TodoList } from '../modules/TodoList/TodoList'
 import './globalStyles.css'
 import BreweriesSimpleLayout from '../modules/BreweriesSimpleLayout/BreweriesSimpleLayout'
 import { ImageGallery } from '../modules/ImageGalleryLayout/ImageGalleryLayout';
+import { TrelloLayout } from '../modules/TrelloLayout/TrelloLayout';
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <>
         {/* <BreweriesSimpleLayout /> */}
-        <ImageGallery />
+        {/* <ImageGallery /> */}
+        <TrelloLayout />
         {/* <TodoList /> */}
       </>
     )
